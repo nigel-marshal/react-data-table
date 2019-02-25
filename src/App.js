77f7d4b4cebde6c79898a6b280bdc357a2a8cbca
Nigel Marshal <nigel.marshal@dubizzle.com>
2019-02-25 17:15:40 +0400

    feat(App): Implemented new design, API call and customisable options as skeletion.(Third time is the charm)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
+    const url = "http://jsonplaceholder.typicode.com/posts";
+    fetch(url, {
+      method: "GET"
+    }).then(response => response.json()).then(posts => {
+      this.setState({posts:posts})
+    })
+
+
+  }
+  
   render() {
+    const columns = [
+      {
+        Header: "User ID",
+        accessor: "id",
+        style: {
+          textAlign: "right"
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100
+      },
+      {
+        Header: "ID",
+        accessor: "userId",
+        style: {
+          textAlign: "right"
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+        sortable: false,
+        filterable: false,
+
+      },
+      {
+        Header: "Content",
+        accessor: "body",
+        sortable: false,
+        filterable: false
+      },
+      {
+        Header: "Actions",
+        Cell: props => {
+          return (
+            <button>Delete</button>
+          )
+        },
+        sortable: false,
+        filterable: false,
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100
+      }
+
+    ]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ReactTable columns={columns}
+        data={this.state.posts}
+        filterable
+        defaultPageSize={5}
+        noDataText={"Loading..."}
+      >
+
+      </ReactTable>
     );
   }
 }
